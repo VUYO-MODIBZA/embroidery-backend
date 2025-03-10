dabbccfb8e4ed6058481e86c01de3253043ef5a2
VUYO-MODIBZA <110651911+VUYO-MODIBZA@users.noreply.github.com>
2025-03-10 13:00:55 +0200

    Update allowedOrigins.js

--- models/config/allowedOrigins.js
+++ models/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
-    'http://localhost:3000',
-    'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com'
+    'http://localhost:3500',
+    'https://dnr-embroidery-and-printing.onrender.com',
+    'dnr-embroidery-and-printing.onrender.com'
 ]
 
 module.exports = allowedOrigins
